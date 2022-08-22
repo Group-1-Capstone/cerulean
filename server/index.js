fef79222bb85923e4df2e5899b3a98f90b6c0f82
Lisa Younes <lyounes@gmail.com>
2022-08-22 14:24:37 -0500

    server-index

--- server/index.js
+++ server/index.js
@@ -4,6 +4,11 @@ const PORT = process.env.PORT || 8080;
 const app = require('./app');
 const seed = require('../seed');
 
+const io = require('socket.io');
+
+// is this needed:
+// require('./socket')(io);
+
 const init = async () => {
   try {
     if (process.env.SEED === 'true') {
@@ -12,7 +17,16 @@ const init = async () => {
       await db.sync();
     }
     // start listening (and create a 'server' object representing our server) socket server will go here too
-    app.listen(PORT, () => console.log(`Mixing it up on port ${PORT}`));
+    const server = app.listen(PORT, () =>
+      console.log(`Mixing it up on port ${PORT}`)
+    );
+
+    const socketServer = new io.Server(server);
+    socketServer.on('connection', (socket) => {
+      socket.on('action', (action) => {
+        socket.broadcast.emit('action', action);
+      });
+    });
   } catch (ex) {
     console.log(ex);
   }
