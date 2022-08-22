b5e0c067a05985a1ca5d72548fb361274f87f4fb
Eve <eveholzgruber@gmail.com>
2022-08-22 21:09:44 +0000

    add jessie sprite

--- client/src/scenes/Main.js
+++ client/src/scenes/Main.js
@@ -7,9 +7,37 @@ export default class Main extends Phaser.Scene {
   preload() {
     this.load.image('red_ball', 'sprites/red_ball.png');
     this.load.image('red_particle', 'sprites/red.png');
+    
+    //jessie
+    
+    // this.load.spritesheet("jessie", jessie, {
+    //   frameWidth: 47,
+    //   frameHeight: 63,
+    // });
   }
 
   create() { 
+
+    //jessie animations
+
+    // this.anims.create({
+    //   key: "turn",
+    //   frames: [{ key: "jessie", frame: 7 }],
+    //   frameRate: 20,
+    // });
+    // this.anims.create({
+    //   key: "right",
+    //   frames: this.anims.generateFrameNumbers("jessie", { start: 3, end: 5 }),
+    //   frameRate: 10,
+    //   repeat: -1,
+    // });
+    // this.anims.create({
+    //   key: "left",
+    //   frames: this.anims.generateFrameNumbers("jessie", { start: 9, end: 11 }),
+    //   frameRate: 10,
+    //   repeat: -1,
+    // });
+    
     const particles = this.add.particles('red_particle');
 
     const emitter = particles.createEmitter({
