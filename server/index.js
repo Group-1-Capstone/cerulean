93743aedf8bc2e12bf900f8d9b775d8dd8f175ac
dcbasham <dauriebasham@gmail.com>
2022-08-23 10:16:50 -0500

    moved Socket client to main

--- server/index.js
+++ server/index.js
@@ -5,8 +5,7 @@ const PORT = process.env.PORT || 8080;
 const app = require('./app');
 const seed = require('../seed');
 const socketListeners = require('./sockets');
-// is this needed:
-// require('./socket')(io);
+/* https://socket.io/docs/v4/client-api/#socketdisconnect */
 
 const init = async () => {
   try {
@@ -21,10 +20,31 @@ const init = async () => {
     const socketServer = new io.Server(server);
     socketServer.on('connection', (socket) => {
       const disconnect = socketListeners(socket);
+      // const socketId = `${socket.id}`;
+      console.log(`Connection from client ${socket.id}`);
     });
   } catch (ex) {
     console.log(ex);
   }
 };
 init();
-// start listening (and create a 'server' object representing our server) socket server will go here too
+// start listening (and create a 'server' object representing our server)
+/* looks like there is already a socket.on 'disconnect' built in function?
+ * https://www.tutorialspoint.com/socket.io/socket.io_hello_world.htm
+ * https://socket.io/docs/v4/server-api/#socketdisconnectclose */
+/* Disconnects this socket. If value of close is true, closes the underlying connection. Otherwise, it just disconnects the namespace.
+
+****
+io.on("connection", (socket) => {
+  setTimeout(() => socket.disconnect(true), 5000);
+});
+****OR
+//Whenever someone connects this gets executed
+io.on('connection', function(socket){
+   console.log('A user connected');
+
+   //Whenever someone disconnects this piece of code executed
+   socket.on('disconnect', function () {
+      console.log('A user disconnected');
+   });
+}); */
--- server/sockets/index.js
+++ server/sockets/index.js
@@ -2,9 +2,32 @@ module.exports = (socket) => {
   socket.on('event', (event) => {
     socket.broadcast.emit('event', event);
   });
+
+  console.log(socket.rooms); // Set { <socket.id> }
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
 };
-// setlisteners set ids put in rooms
+/*
+socket.braodcast.emit() to all connected clients except the sender
+setlisteners set ids put in rooms
 // we need a tear down func return func() on line 5 before close
+/** var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+    res.sendfile('s.html');
+});
+
+io.on('connection', function(socket){
+    console.log('a user connected: ' + socket.id);
+    socket.on('disconnect', function(){
+        console.log( socket.name + ' has disconnected from the chat.' + socket.id);
+    });
+    socket.on('join', function (name) {
+        socket.name = name;
+        console.log(socket.name + ' joined the chat.');
+    });
+}); */
 /*  game logic file server side functions
 when client emits actions send back something to people in that room
 
