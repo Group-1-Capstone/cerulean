a1588c7417a6df2a5efbb36902f3b59626c5d240
Eve <eveholzgruber@gmail.com>
2022-08-31 22:21:18 +0000

    rocks appear and game ends upon hitting one

--- client/src/scenes/GameRoom.js
+++ client/src/scenes/GameRoom.js
@@ -19,6 +19,7 @@ export default class GameRoom extends Phaser.Scene {
       'assets/dino/spritesheetalec.png',
       'assets/dino/spritesheetalec.json'
     );
+    // this.load.image('rock', 'assets/dino/disk-1.png' )
     this.load.image('rock', 'assets/dino/rock.png' )
   }
 
@@ -30,14 +31,11 @@ export default class GameRoom extends Phaser.Scene {
     /* var image = scene.add.tileSprite(x, y, width, height, textureKey); */
     // speed the ground moves in px/second
     this.gameSpeed = 10;
-    // console.log('this', this);
     this.ground = this.add
-      .tileSprite(0, height, width, 26, 'ground')
+      .tileSprite(0, height, width, 32, 'ground') //26
       .setOrigin(0, 1);
 
     this.respawnTime = 0;
-
-    // console.log('this.ground', this.ground);
 
     this.cloudsWhite = this.add.tileSprite(400, 260, 800, 420, 'clouds-white');
     this.cloudsWhiteSmall = this.add.tileSprite(400, 260, 800, 415, 'clouds-white-small');
@@ -51,18 +49,7 @@ export default class GameRoom extends Phaser.Scene {
       }),
       repeat: -1,
     });
-
-    // this.anims.create({
-    //   key: 'jump',
-    //   frames:
-    // });
     
-    
-    // let gameTime = 0
-    // this.scoreText = this.add.text(width, 0, "00000", {fill: "#535353", font: '900 35px Courier', resolution: 5})
-    //   .setOrigin(1, 0)
-    //   .setAlpha(0);
-
     this.player = this.physics.add
       .sprite(0, height, 'characterAtlas')
       .setOrigin(0, 1)
@@ -70,14 +57,6 @@ export default class GameRoom extends Phaser.Scene {
       .setGravityY(5000)
       .play('run');
       
-    // const rock = this.physics.add.image(600, height, 'rock').setOrigin(0, 1)
-      
-    // this.player.addListener('click', () => {
-    //   this.player.setFrame('alec8');
-    // });
-
-
-
     // this.physics.add.collider(this.player, this.ground);
     //this isn't doing anything since this.ground wasn't added with physics
     //and if you physics.add the ground it breaks. 
@@ -85,15 +64,16 @@ export default class GameRoom extends Phaser.Scene {
     this.obsticles = this.physics.add.group();
     
     this.physics.add.collider(this.player, this.obsticles, () => {
-      // this.highScoreText.x = this.scoreText.x - this.scoreText.width - 20;
 
-      // const highScore = this.highScoreText.text.substr(this.highScoreText.text.length - 5);
-      // const newScore = Number(this.scoreText.text) > Number(highScore) ? this.scoreText.text : highScore;
-
-      // this.highScoreText.setText('HI ' + newScore);
-      // this.highScoreText.setAlpha(1);
-
+      const gameOverText = this.add.text(400, 300, "The End", {
+          fontSize: "64px",
+          fill: "#EE3D73",  //font color
+        });
+        
+      //TODO: make everything stop, not just alec
+      //he only stops if he's in the air, he keeps walking after colliding
       this.physics.pause();
+      this.player.anims.stop();
       // this.isGameRunning = false;
       // this.anims.pauseAll();
       // this.respawnTime = 0;
@@ -103,29 +83,10 @@ export default class GameRoom extends Phaser.Scene {
       // this.hitSound.play();
     }, null, this);
     
+    this.score = 0
     
-    // console.log("player", this.player)
-    // console.log("body", this.player.body)
-    // console.log("blocked down", this.player.body.blocked.down)
-    // console.log("touching", this.player.body.touching.down)
-    // console.log("onFloor", this.player.body.onFloor())
-    
-    
-    // this works console.log("width", this.game.config.width)
-    // const rock = this.physics.add.image(this.game.config.width, this.game.config.height, 'rock')
-    //   .setOrigin(0, 1);
-
-    // rock.body.offset.y = +10;
-    
-    // this.handleInputs()
+    //end of create func
   }
-
-  /* this.input.pointer //(property) globalThis.Phaser.Input.InputPlugin.pointer1: Phaser.Input.Pointer
-    A touch-based Pointer object. This will be undefined by default unless you add a new Pointer using addPointer. */
- 
-  // handleInputs() {
-  //   console.log("inputs invoked")
-  // }
 
   placeObsticle() {
     // change obsticleNum to match our num of obstacles
@@ -134,7 +95,8 @@ export default class GameRoom extends Phaser.Scene {
     
     //do an array and create obstacle[random index] instead of tutorial naming system
     
-    const distance = Phaser.Math.Between(this.game.config.width - 200);
+    const distance = Phaser.Math.Between(600,900);
+    //his screen is wider, maybe use this.game.config.width
 
     let obsticle;
     // if (obsticleNum > 6) {
@@ -155,6 +117,7 @@ export default class GameRoom extends Phaser.Scene {
     //     .setOrigin(0, 1);
         
     obsticle = this.obsticles.create(this.game.config.width + distance, this.game.config.height, 'rock')
+        .setScale(0.5)
         .setOrigin(0, 1);
 
      obsticle.body.offset.y = +10;
@@ -163,52 +126,42 @@ export default class GameRoom extends Phaser.Scene {
   }
 
   // 60fps
-  update(delta) {
+  update(time, delta) {
+    //see if we need time param - yes, but why?
+    //delta is the time from the last frame
     
-    this.cloudsWhite.tilePositionX += 0.5;
-    this.cloudsWhiteSmall.tilePositionX += 0.25;
+    this.cloudsWhite.tilePositionX += 0.25;
+    this.cloudsWhiteSmall.tilePositionX += 0.5;
+    
     // every update the ground tile will move forward by this amt
     this.ground.tilePositionX += this.gameSpeed;
+    
     Phaser.Actions.IncX(this.obsticles.getChildren(), -this.gameSpeed);
     // Takes an array of Game Objects, or any objects that have a public x property, and then adds the given value to each of their x properties.
     
     this.respawnTime += delta * this.gameSpeed * 0.08;
+    // this.gameSpeed += 0.01
+    
     if (this.respawnTime >= 1500) {
+      //1500 ms
       this.placeObsticle();
       this.respawnTime = 0;
     }
     
     if(this.input.activePointer.isDown) {
       if (!this.player.body.onFloor()) {
-        // console.log("onfloor in air", this.player.body.onFloor())
-        return; }
-        //works the same without checking x velocity > 0 
-      // if (!this.player.body.onFloor() || this.player.body.velocity.x > 0) { return; }
-      
-      // console.log("onfloor, jump", this.player.body.onFloor())
-      // this.player.setFrame('alec8'); //makes him stop on the frame while mouse is held
-      this.player.setVelocityY(-1600);
-       
-      //TODO: set him to 1 frame in air 
-      // this.player.texture.frames['alec8']; //doesnt work
-      
-    
-      
-    }
-      if (this.player.body.deltaAbsY() > 0) {
-        //while in air
-        this.player.anims.stop();
-        this.player.setFrame('alec8');
-      } else {
-        this.player.play('run', true)
+        return;
       }
+      this.player.setVelocityY(-1600); //-1600
+    }
     
-    
-    // const cursors = this.input.keyboard.createCursorKeys();
-    // if (cursors.up.isDown && this.player.body.touching.down) {
-    //   console.log("test")
-    //   this.player.setVelocityY(-1600); //-420 //jump height
-    // }
+    if (this.player.body.deltaAbsY() > 0) {
+        //while in air
+      this.player.anims.stop();
+      this.player.setFrame('alec8');
+    } else {
+      this.player.play('run', true);
+    }
     
   }
   
