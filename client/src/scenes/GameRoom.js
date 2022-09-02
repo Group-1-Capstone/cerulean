8979568c6a5067677e70045bc438c2584b03eacf
dcbasham <dauriebasham@gmail.com>
2022-09-02 14:44:49 -0500

    gameRoom fixes

--- client/src/scenes/GameRoom.js
+++ client/src/scenes/GameRoom.js
@@ -30,13 +30,11 @@ export default class GameRoom extends Phaser.Scene {
   create() {
     const div = document.getElementById('gameContainer');
     this.add.image(400, 300, 'sky');
-    // canvas height and width
     const { height, width } = this.game.config;
-    /* var image = scene.add.tileSprite(x, y, width, height, textureKey); */
-    // speed the ground moves in px/second
+
     this.gameSpeed = 10;
     this.ground = this.add
-      .tileSprite(0, height, width, 32, 'ground') //26
+      .tileSprite(0, height, width, 32, 'ground')
       .setOrigin(0, 1);
 
     this.respawnTime = 0;
@@ -67,10 +65,6 @@ export default class GameRoom extends Phaser.Scene {
       .setGravityY(5000)
       .play('run');
 
-    // this.physics.add.collider(this.player, this.ground);
-    //this isn't doing anything since this.ground wasn't added with physics
-    //and if you physics.add the ground it breaks.
-
     this.obsticles = this.physics.add.group();
 
     this.physics.add.collider(
@@ -83,27 +77,10 @@ export default class GameRoom extends Phaser.Scene {
         });
         this.gameOver = true;
         this.player.anims.stop();
-        //TODO: make everything stop, not just alec
-        //he only stops if he's in the air, he keeps walking after colliding
-        //this.physics.pause();
-
-        // this.player.anims.stop();
-        // this.cloudsWhite.anims.stop();
-        // this.cloudsWhiteSmall.anims.stop();
-
-        // this.isGameRunning = false;
-        // this.anims.pauseAll();
-        // this.respawnTime = 0;
-        // this.gameSpeed = 10;
-        // this.gameOverScreen.setAlpha(1);
-        // this.score = 0;
-        // this.hitSound.play();
       },
       null,
       this
     );
-
-    this.score = 0;
 
     const exitButton = this.add
       .image(50, 50, 'exitButton')
@@ -134,35 +111,17 @@ export default class GameRoom extends Phaser.Scene {
       },
       this
     );
-    //end of create func
+    this.score = 0;
+    this.scoreText = this.add.text(500, 400, `Score: ${this.score}`, {
+      fontSize: '24px',
+      fill: '#BFF0D4',
+    });
   }
 
   placeObsticle() {
-    // change obsticleNum to match our num of obstacles
-    // e.g. Math.floor(Math.random() * 7) is 0 through 6.
-    // const obsticleNum = Math.floor(Math.random() * 7) + 1;
-
-    //do an array and create obstacle[random index] instead of tutorial naming system
-
     const distance = Phaser.Math.Between(600, 900);
-    //his screen is wider, maybe use this.game.config.width
 
     let obsticle;
-    // if (obsticleNum > 6) {
-    //   const enemyHeight = [20, 50];
-    //   obsticle = this.obsticles.create(this.game.config.width + distance, this.game.config.height - enemyHeight[Math.floor(Math.random() * 2)], `enemy-bird`)
-    //     .setOrigin(0, 1)
-    //     obsticle.play('enemy-dino-fly', 1);
-    //   obsticle.body.height = obsticle.body.height / 1.5;
-    // } else {
-    //   obsticle = this.obsticles.create(this.game.config.width + distance, this.game.config.height, `obsticle-${obsticleNum}`)
-    //     .setOrigin(0, 1);
-
-    // obsticle.body.offset.y = +10;
-    // }
-
-    // obsticle = this.obsticles.create(600, 600, 'rock')
-    //     .setOrigin(0, 1);
 
     obsticle = this.obsticles
       .create(
@@ -178,11 +137,13 @@ export default class GameRoom extends Phaser.Scene {
     obsticle.setImmovable();
   }
 
-  // 60fps
   update(time, delta) {
-    //see if we need time param - yes, but why?
-    //delta is the time from the last frame
-    if (this.gameOver !== true) {
+    if (this.gameOver) {
+      return;
+    }
+    this.score++;
+    this.scoreText.setText(`Score: ${this.score}`);
+
       this.cloudsWhite.tilePositionX += 0.25;
       this.cloudsWhiteSmall.tilePositionX += 0.5;
 
@@ -196,7 +157,6 @@ export default class GameRoom extends Phaser.Scene {
       // this.gameSpeed += 0.01
 
       if (this.respawnTime >= 1500) {
-        //1500 ms
         this.placeObsticle();
         this.respawnTime = 0;
       }
@@ -205,13 +165,13 @@ export default class GameRoom extends Phaser.Scene {
         if (!this.player.body.onFloor()) {
           return;
         }
-        this.player.setVelocityY(-1600); //-1600
+        this.player.setVelocityY(-1600);
       }
 
       if (this.player.body.deltaAbsY() > 0) {
         //while in air
         this.player.anims.stop();
-        //this.player.setFrame('alec8');
+        this.player.setFrame('alec8');
       } else {
         this.player.play('run', true);
       }
